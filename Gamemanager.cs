e6222f6e5f9ac87785236ed4a5ec7bd9d801d97e
nakyung128 <68213689+nakyung128@users.noreply.github.com>
2020-08-14 21:02:49 +0900

    Add files via upload

---
+++ Gamemanager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gamemanager : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject EndPanel;
+
+    private void Start()
+    {
+        if (!DataManager.Instance.PlayerDie)
+        {
+
+        }
+
+        if (DataManager.Instance.PlayerDie == true)
+        {
+            EndPanel.SetActive(true);
+        }
+        
+    }
+    
+    public void Restart_Btu()
+    {
+        DataManager.Instance.score = 0;
+        DataManager.Instance.PlayerDie = false;
+        DataManager.Instance.PlayerTimeCurrent = DataManager.Instance.playTimeMax;
+        DataManager.Instance.margnetTimeCurrent = 0;
+
+        SceneManager.LoadScene("SampleScene");
+    }
+}
---
+++ Player.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag.CompareTo("Land") == 0)
+        {
+            jumpCount = 0;
+        }
+
+        if (collision.gameObject.tag.CompareTo("Block") == 0)
+        {
+            DataManager.Instance.playTimeCurrent -= 2f;
+        }
+    }
+}
